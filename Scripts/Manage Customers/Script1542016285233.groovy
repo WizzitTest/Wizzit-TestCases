8b2cbd5191ffbd3dff6e0b73d7770a3d6689615c
violetmodise <violetmodise@10.18.2.16>
2018-11-15 09:22:14 +0200

    Can we work on this from now on

--- Scripts/Manage Customers/Script1542016285233.groovy
+++ Scripts/Manage Customers/Script1542016285233.groovy
@@ -17,7 +17,7 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl('http://ubank.wizzitcloud.com:9094/#/login')
 
-WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Username_input_0'), 'admin')
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Username_input_0'), 'pat')
 
 WebUI.setEncryptedText(findTestObject('Object Repository/Page_esbCloudPortal/input_Password_input_1'), 'RigbBhfdqOBGNlJIWM1ClA==')
 
@@ -27,21 +27,89 @@ WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Login'))
 
 WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage customers'))
 
-WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/button_customers'))
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_customers'))
 
 WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-select_Search ByMobile numb'))
 
-WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/div_Name'))
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/div_Mobile number'))
 
-WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Filter_input_12'), 'violet')
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Filter_input_12'), '0633077666')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Search'))
+
+WebUI.waitForElementClickable(findTestObject('Page_esbCloudPortal/span_Manage'), 5)
+
+WebUI.mouseOver(findTestObject('Page_esbCloudPortal/span_Manage'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage'))
+
+WebUI.waitForElementClickable(findTestObject('Page_esbCloudPortal/span_Edit Customer'), 5)
+
+WebUI.doubleClick(findTestObject('Object Repository/Page_esbCloudPortal/span_Edit Customer'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Surname_Surname'), 'SIBINDI')
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Name_Name'), 'NELSON')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Update'))
+
+WebUI.mouseOver(findTestObject('Page_esbCloudPortal/span_Ok'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok'))
+
+WebUI.waitForElementClickable(findTestObject('Page_esbCloudPortal/span_Supervisor approvals'), 5)
+
+WebUI.doubleClick(findTestObject('Object Repository/Page_esbCloudPortal/span_Supervisor approvals'))
+
+WebUI.waitForElementClickable(findTestObject('Page_esbCloudPortal/span_Pending updates'), 5)
+
+WebUI.doubleClick(findTestObject('Object Repository/Page_esbCloudPortal/span_Pending updates'))
+
+WebUI.mouseOver(findTestObject('Page_esbCloudPortal/span_CUSTOMER'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_CUSTOMER'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_SIBINDI_ng-scope md-fo'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Supervisor approvals_m'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Supervisor approvals_m'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/button_Johan Vorster'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Logout'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Username_input_19'), 'pat')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_esbCloudPortal/input_Password_input_20'), 'RigbBhfdqODKcAsiUrg+1Q==')
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Organisation_input_21'), 'ubank')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Login'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_OK (1)'))
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_esbCloudPortal/input_Password_input_20'), 'RigbBhfdqOBGNlJIWM1ClA==')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Login'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage customers'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_customers'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-select_Search ByMobile numb_1'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/div_Mobile number'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Filter_input_32'), '0767925474')
 
 WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Search'))
 
 WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage'))
 
-WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Edit Customer'))
-
-WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Name_Name'), 'VIOLET MODISE')
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Change Mobile Number'))
 
 WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Update'))
 
@@ -51,7 +119,181 @@ WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Superviso
 
 WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Pending updates'))
 
-WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_UBANKP001_ng-scope md-'))
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_CUSTOMER'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_msisdn_ng-scope md-fon'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Supervisor approvals_m'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/button_Johan Vorster'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Logout'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Username_input_0'), 'pat')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_esbCloudPortal/input_Password_input_1'), 'RigbBhfdqOBGNlJIWM1ClA==')
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Organisation_input_2'), 'ubank')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Login'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage customers'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_customers'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-select_Search ByMobile numb'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/div_Mobile number'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Filter_input_12'), '0633077666')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Search'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Banking Services'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-radio-button_Internet Banki'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Activate'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Supervisor approvals'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Pending banking services'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_INTERNET_BANKING_ng-sc'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_OK (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Supervisor approvals_m'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Manage customers_menu-'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Johan Vorster'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/button_Logout'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Username_input_0'), 'pat')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_esbCloudPortal/input_Password_input_1'), 'RigbBhfdqOBGNlJIWM1ClA==')
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Organisation_input_2'), 'ubank')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Login'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage customers'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_customers'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-select_Search ByMobile numb'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/div_Mobile number'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Filter_input_12'), '0633077666')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Search'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Block Channels'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-radio-button_Internet Banki_1'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Block'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Re-Issue Pin'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-radio-button_Internet Banki_2'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Re-issue'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_View Links'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/input'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Supervisor approvals'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Pending accounts'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_DERE LINK ACCOUNT'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_uSave-Fixed Deposit_ng'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_OK (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Supervisor approvals_m'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Manage customers_menu-'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/button_Johan Vorster'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Logout'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Username_input_56'), 'pat')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_esbCloudPortal/input_Password_input_57'), 'RigbBhfdqOBGNlJIWM1ClA==')
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Organisation_input_58'), 'ubank')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Login'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage customers'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_customers'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-select_Search ByMobile numb_2'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/div_Mobile number'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Filter_input_68'), '0633077666')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Search'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Updates Limits'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_MONTH_ng-pristine ng-unt'), '8')
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_MONTH_ng-untouched ng-va'), '8')
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_TRAN_ng-pristine ng-unto'), '7')
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_TRAN_ng-untouched ng-val'), '7')
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_TRAN_ng-pristine ng-unto'), '6')
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_TRAN_ng-untouched ng-val'), '6')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Update'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Supervisor approvals'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Pending updates'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_CUSTOMER'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Internal Transfers Mon'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Supervisor approvals_m'))
 
 WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Johan Vorster'))
 
--- Scripts/Manage Users/Script1542016309306.groovy
+++ Scripts/Manage Users/Script1542016309306.groovy
@@ -17,7 +17,7 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl('http://ubank.wizzitcloud.com:9094/#/login')
 
-WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Username_input_0 (1)'), 'admin')
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Username_input_0 (1)'), 'pat')
 
 WebUI.setEncryptedText(findTestObject('Object Repository/Page_esbCloudPortal/input_Password_input_1 (1)'), 'RigbBhfdqOBGNlJIWM1ClA==')
 
@@ -27,33 +27,155 @@ WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Login (1)
 
 WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage Users'))
 
-WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/button_users'))
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_users'))
 
-WebUI.waitForElementClickable(findTestObject('Page_esbCloudPortal/button_Manage'), 5)
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-select_Search ByAll UsersMo'))
 
-WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/button_Manage'))
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/div_Mobile Number (1)'))
 
-WebUI.waitForElementClickable(findTestObject('Page_esbCloudPortal/span_Edit User'), 5)
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Filter_input_12 (1)'), '0633077666')
 
-WebUI.doubleClick(findTestObject('Object Repository/Page_esbCloudPortal/span_Edit User'))
-
-WebUI.waitForElementClickable(findTestObject('Page_esbCloudPortal/span_Cancel'), 5)
-
-WebUI.doubleClick(findTestObject('Object Repository/Page_esbCloudPortal/span_Cancel'))
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Search (1)'))
 
 WebUI.waitForElementClickable(findTestObject('Page_esbCloudPortal/span_Manage (1)'), 5)
 
-WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage (1)'))
+WebUI.mouseOver(findTestObject('Page_esbCloudPortal/span_Manage (1)'))
+
+WebUI.click(findTestObject('Page_esbCloudPortal/span_Manage (1)'))
+
+WebUI.waitForElementClickable(findTestObject('Page_esbCloudPortal/span_Add Portal Channel'), 5)
+
+WebUI.mouseOver(findTestObject('Page_esbCloudPortal/span_Add Portal Channel'))
 
 WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Add Portal Channel'))
 
-WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Cancel'))
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Employee id_input_43'), 'P77765')
 
-WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Add User'))
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Branch ID_input_44'), 'Sandton')
 
-WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Johan Vorster (1)'))
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_E-mail_input_45'), 'vtmodise@gmail.com')
 
-WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/button_Logout'))
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_esbCloudPortal/select_AuditCall_CenterFinance'), 'string:58e2a84e42e4142f7a0ef8a8',
+	true)
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/div_Select Role_md-off'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Activate (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok (2)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Supervisor approvals (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Pending banking services (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_PORTAL_ng-scope md-fon'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_OK (3)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Supervisor approvals_m (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Manage Users_menu-togg'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/button_Johan Vorster (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Logout (1)'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Username_input_0 (1)'), 'pat')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_esbCloudPortal/input_Password_input_1 (1)'), 'RigbBhfdqOBGNlJIWM1ClA==')
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Organisation_input_2 (1)'), 'ubank')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Login (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage Users'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_users'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-select_Search ByAll UsersMo'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/div_Username'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Filter_input_12 (1)'), 'P98765')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Search (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Edit User'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Username_CellNo'), 'P56879')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Update (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok (2)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Supervisor approvals (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Pending updates (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_P56879_ng-scope md-fon'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_P56879_ng-scope md-fon'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Supervisor approvals_m (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-icon_Manage Users_menu-togg'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/button_Johan Vorster (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Logout (1)'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Username_input_0 (1)'), 'pat')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_esbCloudPortal/input_Password_input_1 (1)'), 'RigbBhfdqOBGNlJIWM1ClA==')
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Organisation_input_2 (1)'), 'ubank')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Login (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage Users'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_users'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/md-select_Search ByAll UsersMo'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/div_Username'))
+
+WebUI.setText(findTestObject('Object Repository/Page_esbCloudPortal/input_Filter_input_12 (1)'), 'P65432')
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Search (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_RE-Issue Portal Password'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/div_Processing Request..._md-o'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Re-issue (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok (2)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_De-activate Portal user'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/div_Processing Request..._md-o_1'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Deactivate'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok (2)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Manage (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Change user roles'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_esbCloudPortal/select_AuditCall_CenterFinance'), 'string:58e2a84e42e4142f7a0ef8a8',
+	true)
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Save'))
+
+WebUI.click(findTestObject('Object Repository/Page_esbCloudPortal/span_Ok (2)'))
 
 WebUI.closeBrowser()
 
